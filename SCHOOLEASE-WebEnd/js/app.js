0aeb49e2d66ed3236875eaa663ecb72defb28987
kenfackyann <yannkenfack3@gmail.com>
2024-07-20 11:33:16 +0100

    modifying order of pages and modifying my section

--- SCHOOLEASE-WebEnd/js/app.js
+++ SCHOOLEASE-WebEnd/js/app.js
@@ -3,11 +3,10 @@ async function includeHTML(elementId,file) {
     const htmlContent = await response.text();
     document.getElementById(elementId).innerHTML = htmlContent;
 }
-
+includeHTML('goldsmith','/pages/smith_pages.html');
 includeHTML('yann','/pages/yann_page.html');
 includeHTML('ange','/pages/ange_page.html');
 includeHTML('lelica','/pages/lelica_page.html');
-includeHTML('goldsmith','/pages/smith_pages.html');
-includeHTML('jeanClaude','/pages/smith_pages.html');
-includeHTML('maisy','/pages/smith_pages.html');
-includeHTML('tessoh','/pages/smith_pages.html');
+includeHTML('jeanClaude','/pages/s');
+includeHTML('maisy','/pages/s');
+includeHTML('tessoh','/pages/s');
