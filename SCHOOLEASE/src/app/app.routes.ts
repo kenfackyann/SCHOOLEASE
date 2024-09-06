df697f5d0728b6132fc28d469d5c9b925e31dd5e
kenfackyann <yannkenfack3@gmail.com>
2024-09-06 04:30:45 +0100

    adding the new pages

--- SCHOOLEASE/src/app/app.routes.ts
+++ SCHOOLEASE/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from "./features/home/home.component";
+import {SchoolsComponent} from "./features/schools/schools.component"
 
 export const routes: Routes = [
   {
     path: '', component: HomeComponent, title: 'Home'
+  },
+  {
+    path:'school', component:SchoolsComponent, title:'schools'
   }
 ];
--- SCHOOLEASE/src/app/features/home/home.component.ts
+++ SCHOOLEASE/src/app/features/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../../shared/components/header/header.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    RouterLink
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+
+  public showChatbot(){
+    console.log('sort')
+  }
 
 }
